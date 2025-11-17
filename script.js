d0ae1ca49ec467a75041359358cac12cb44b288d
Gustavo Ftz <gustavoftz12@outlook.com>
2025-11-17 19:26:19 -0300

    update files

---
+++ script.js
@@ -0,0 +1,3 @@
+alert(
+  "Aviso! Este projeto é um clone não oficial da interface da Netflix, desenvolvido apenas para fins de estudo e portfólio. Não é um produto oficial da Netflix. Não possui qualquer vínculo com a Netflix, Inc. Não há qualquer finalidade comercial ou lucrativa. Todas as marcas registradas e logotipos pertencem aos seus respectivos proprietários. "
+);
